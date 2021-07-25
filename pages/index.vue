1839079af80bce852073690626e215d2623d1557
spjy <spjy@hawaii.edu>
2021-07-25 10:34:36 -0700

    feat: create arcgis component

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,42 @@
-  <style>
-    html,
-    body,
-    #viewDiv {
-      padding: 0;
-      margin: 0;
-      height: 100%;
-      width: 100%;
-    }
+<template>
+  <div>
+    <div id="info" class="esri-widget">
+      <camera-info></camera-info>
+    </div>
+  </div>
+</template>
 
-    #info {
-      padding: 1em;
-    }
-  </style>
-  <script>
-    import Camera from '@arcgis/core/Camera';
 
-    window.dojoConfig = {
-      packages: [
-        {
-          name: "vue",
-          location: "https://unpkg.com/vue/dist/",
-          main: "vue"
-        }
-      ]
-    };
-    // Initialize the view with a specific camera
+<script>
+  import Camera from '@arcgis/core/Camera';
 
-    export default {
-      data () { return { cam: new Camera({
+  export default {
+    data () { 
+      return {
+        cam: new Camera({
           heading: 90, // face due east
           tilt: 45, // looking from a bird's eye view
           position: [ -122, 38, 20000 ]  // creates a point instance (x,y,z)
-        }) } }
+        }) 
+      } 
     }
-  </script>
+  }
+</script>
 
+<style>
+  html,
+  body,
+  #viewDiv {
+    padding: 0;
+    margin: 0;
+    height: 100%;
+    width: 100%;
+  }
 
-<template>
-  <div>
-<div id="info" class="esri-widget">
-  <camera-info v-bind:cam="cam"></camera-info>
-</div>
-<div id="viewDiv"></div>
-</div>
-</template>
-
+  #info {
+    padding: 1em;
+  }
+</style>
 
 <!-- This is the arcgis thing a ma jig -->
 
